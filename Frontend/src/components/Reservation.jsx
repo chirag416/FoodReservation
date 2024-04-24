74cf046c9d136f16ae789d93d5ffb333c11b269e
chirag416 <chiragyadav416@gmail.com>
2024-04-24 21:46:29 +0530

    updated backend url

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://lonely-ruby-firefly.cyclic.app/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
