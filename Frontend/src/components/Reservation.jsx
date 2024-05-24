82f892406fe972b1e2bfc9cb9a4b22a5486f1ce8
chirag416 <141557512+chirag416@users.noreply.github.com>
2024-05-24 18:17:13 +0530

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://mern-backend-ua1w.onrender.com/api/v1/reservation/send",
+        "https://foodr-backend.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
